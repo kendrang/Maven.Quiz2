940d55ffc3640a825e338179c29d3a9382bbdc06
kendrang <kendrang@users.noreply.github.com>
2019-11-04 10:05:06 -0500

    part 2 of 1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,11 +2,24 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if (array.length % 2 ==0){
+            return true;
+        }
+
+        else return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer [] answer = new Integer[(stop-start)+1];
+        answer[0]= start;
+
+            for (int j = 0 ; j < answer.length; j ++) {
+
+                answer[j] = start ++;
+
+        }
+
+        return answer;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
