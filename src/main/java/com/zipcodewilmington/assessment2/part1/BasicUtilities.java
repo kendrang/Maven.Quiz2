bbee694d5ba71d07bd41f2a0441b8a7f790ceaf9
kendrang <kendrang@users.noreply.github.com>
2019-11-04 09:47:02 -0500

    part 1 done

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,34 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value >= 5){
+        return true; }
+       else  return false;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        if (value <= 7 ){
+            return true;
+        }
+        else return false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if (5 <= valueToEvaluate && valueToEvaluate <= 7){
+            return true;
+        }
+        else return false;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+         Character temp = string.charAt(0);
+         String charHolder = character.toString();
+         boolean answer = temp.toString().equalsIgnoreCase(charHolder);
+
+        if (string.charAt(0) == character || answer == true){
+
+            return true;
+        }
+        return false;
     }
 }
